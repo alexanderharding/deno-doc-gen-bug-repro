16072c6cb690919283e95e7867ab6068f8490b74
Alexander Harding <harding1996@gmail.com>
2026-02-10 16:39:56 -0700

    example with repro

---
+++ docs/search_index.js
@@ -0,0 +1,14 @@
+(function () {
+  window.DENO_DOC_SEARCH_INDEX = {
+    "kind": "search",
+    "nodes": [{
+      "id": "namespace_ExampleConstructor",
+      "kind": [{ "char": "I", "kind": "Interface", "title": "Interface" }],
+      "name": "ExampleConstructor",
+      "file": ".",
+      "doc": "",
+      "url": "././~/ExampleConstructor.html",
+      "deprecated": false,
+    }],
+  };
+})();
---
+++ main.ts
@@ -0,0 +1,8 @@
+export interface ExampleConstructor {
+  /**
+   * Example constructor
+   * @param {number} a - The first number
+   * @returns {object} - The example object
+   */
+  new (a: number): object;
+}
