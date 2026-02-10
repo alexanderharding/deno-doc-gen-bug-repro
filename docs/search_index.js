cf9ceec1e856bd4b88f46bd14c8cc58aa2ba492c
Alexander Harding <harding1996@gmail.com>
2026-02-10 16:55:50 -0700

    Add example

--- docs/search_index.js
+++ docs/search_index.js
@@ -1,14 +1,3 @@
 (function () {
-  window.DENO_DOC_SEARCH_INDEX = {
-    "kind": "search",
-    "nodes": [{
-      "id": "namespace_ExampleConstructor",
-      "kind": [{ "char": "I", "kind": "Interface", "title": "Interface" }],
-      "name": "ExampleConstructor",
-      "file": ".",
-      "doc": "",
-      "url": "././~/ExampleConstructor.html",
-      "deprecated": false,
-    }],
-  };
-})();
+  window.DENO_DOC_SEARCH_INDEX = {"kind":"search","nodes":[{"id":"namespace_ExampleConstructor","kind":[{"char":"I","kind":"Interface","title":"Interface"}],"name":"ExampleConstructor","file":".","doc":"","url":"././~/ExampleConstructor.html","deprecated":false}]};
+})()
--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ export interface ExampleConstructor {
    * Example constructor
    * @param {number} a - The first number
    * @returns {object} - The example object
+   * @example
+   * ```ts
+   * const example = new ExampleConstructor(1);
+   * ```
    */
   new (a: number): object;
 }
